ac60c5b6e6336a2e7e45c1d9f8ea2af548ce4be1
cdaringe <cdaringe@gmail.com>
2021-01-18 10:11:51 -0800

    feat: init wip

---
+++ src/mod.ts
@@ -0,0 +1,19 @@
+const a = [1, 2, 3];
+
+const sum = (a: number[]) => a.reduce((t, x) => t + x, 0);
+
+const doWork = () => {
+  log(sum(a));
+};
+
+log(defines.alert_type.entity_under_attack);
+game.auto_save("ok");
+game.connected_players.forEach((v) => localised_print(v));
+script.on_event(
+  "ar",
+  () => {
+    return "k";
+  },
+  null
+);
+doWork();
